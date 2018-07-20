76850ec34012ffb92fda26ab079629dfa72e6c72
Christopher Vaughn <vaughnc@yosemite.edu>
2018-07-19 18:50:19 -0700

    Minor spelling fix

---
+++ ESP32 oLED DHT11 Weather Sensor Readout/ESP32 oLED DHT11 Weather Sensor Readout.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP32_oLED_DHT11_Weather_Sensor_Readout.ino
+ Created:	7/5/2018 12:08:05 AM
+ Author:	Christopher Vaughn
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
